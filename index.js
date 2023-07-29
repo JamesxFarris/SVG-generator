bce3ac4ada1f80448411226cd468c3a93ef93295
James Farris <jafarris.exe@gmail.com>
2023-07-28 23:49:24 -0400

    Added inquirer scripts for user

--- index.js
+++ index.js
@@ -17,8 +17,46 @@ const questions = [
     },
   },
   //   Prompt for text color
-
+  {
+    type: "input",
+    name: "color",
+    message: "What color would you like your text to be?",
+  },
   // Promt for shape (list)
-
+  {
+    type: "list",
+    name: "shape",
+    message: "What shape would you like to make your logo?",
+    choices: ["Triangle", "Square", "Circle"],
+  },
   // Prompt for shape color
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "What color would you like your shape to be?",
+  },
 ];
+
+// Prompt user for input
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    let shape;
+    if (answers.shape === "Triangle") {
+      shape = new Triangle(answers.shapeColor);
+    } else if (answers.shape === "Square") {
+      shape = new Square(answers.shapeColor);
+    } else if (answers.shape === "Circle") {
+      shape = new Circle(answers.shapeColor);
+    }
+
+    // Write the shape to the file
+    fs.writeFile(
+      "logo.svg",
+      shape.create(answers.name, answers.color),
+      (err) => {
+        if (err) throw err;
+        console.log("You have a new logo!");
+      }
+    );
+  });
+}
